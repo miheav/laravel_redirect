79a7f8ed4219e2dfa9f645cb1937336503986a67
Aleksey Miheev <aleksvalmix@yandex.ru>
2020-11-27 11:42:25 +0500

    fix

--- app/UrlModel.php
+++ app/UrlModel.php
@@ -12,25 +12,30 @@ class UrlModel extends Model
 
     }
 
-    public static $keys = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    public static $keys = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
     public function generateHash(){
 
         if(!preg_match("#^http\w?\://.+?\.[a-z]+$#iu", $this->url))
             return "false";
-        $string = '';
         $quantitySymb = count(self::$keys) - 1;
-        for($i=0; $i<6; $i++) {
-            $string.=self::$keys[random_int(0,$quantitySymb)];
-        }
-        $string = ("http://laravel5.tz/?redir=").$string;
+        do {
+            $string = '';
+            for($i=0; $i<6; $i++) {
+                $string.=self::$keys[random_int(0,$quantitySymb)];
+            }
+            $string = ("http://laravel5.tz/?redir=").$string;
+        } while($this->where('short_url', '=', $string)->first());
+
         if($model = $this->where('url', '=', $this->url)->first()){
             return $model->short_url;
         } else {
+
             $this->short_url = $string;
             $this->save();
             return $this->short_url ;
         }
+
     }
 
 
