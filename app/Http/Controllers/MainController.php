bc345c833b0157b97f21eff7de21f8d7059dc4e4
Aleksey Miheev <aleksvalmix@yandex.ru>
2020-11-27 12:04:23 +0500

    fix

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,11 +4,12 @@
  * @Author: aleks
  * @Date:   2020-11-11 07:55:32
  * @Last Modified by:   aleks
- * @Last Modified time: 2020-11-27 11:13:47
+ * @Last Modified time: 2020-11-27 12:03:45
  */
 namespace App\Http\Controllers;
 use App\UrlModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 /**
  *
  */
@@ -20,15 +21,17 @@ class MainController extends Controller
         $url = $request->get('url');
         $redir = $request->get('redir');
         if($redir) {
-            return Redirect::to($redir);
+            $model = UrlModel::where('short_url', '=', $redir)->first();
+
+            return Redirect::to($model->url);
         }
 
         if(!$url) {
             $result = '';
             return view('welcome',['url' => '','result' => $result]);
         } else {
-            $model = new UrlModel($url);
-            $result = $model->generateHash();
+            $model = new UrlModel();
+            $result = $model->generateHash($url);
             return view('welcome',['url' => $url,'result' => $result]);
         }
     }
